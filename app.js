534793b0fcc393150e4748596262046fac68f8ac
sudhakar reddy <sudhakar.balreddy@mindtree.com>
2017-02-23 15:09:49 +0530

    fixed config file to run code in diff envs

--- app.js
+++ app.js
@@ -2,15 +2,28 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var home = require('./apis');
 var app = express();
-
+var config = require('./config');
 //app.engine('html', ejs.renderFile); helpful in using html template engine
 //app.set('view engine','html'); helpful in using html template engine
 
+app.set('mode',config.mode);
 app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 //app.use('/', home);
 app.use('/api', home);
+
+switch(app.get('mode')) {
+    case 'production' : 
+        console.log(app.get('mode') + ' mode activated');
+        break;
+    case 'qa' : 
+        console.log(app.get('mode') + ' mode activated');
+        break;
+    case 'dev' : 
+        console.log(app.get('mode') + ' mode activated');
+        break;
+}
 
 module.exports = app;
 
--- config/index.js
+++ config/index.js
@@ -1,16 +1,39 @@
-var config = {
-    local : {
-        mode : 'local',
-        port : 3000
+//config file is helpful in running code in different environments
+//NODE_ENV should be set in heroku settings otherwise it will return null
+var config = {};
+var configObj = {
+    dev : {
+        mode : process.env.NODE_ENV || 'dev',
+        port : process.env.PORT || 3000
     },
     qa : {
-        mode : 'qa',
-        port : 4000
+        mode : process.env.NODE_ENV || 'qa',
+        port : process.env.PORT || 4000
     },
     production : {
-        mode : 'production',
-        port : 5000
+        mode : process.env.NODE_ENV || 'production',
+        port : process.env.PORT || 5000
     },
 };
+
+if(process.argv[2] === undefined || process.argv[2] === null){
+    if(process.env.NODE_ENV){
+        config.port = process.env.PORT;
+        config.mode = process.env.NODE_ENV;
+    }
+    else {
+        config.port = configObj.dev.port;
+        config.mode = configObj.dev.mode;
+    }
+}
+else if(process.argv[2] === 'dev' || process.argv[2] === 'qa' || process.argv[2] === 'production') {
+    config.port = configObj[process.argv[2]].port;
+    config.mode = configObj[process.argv[2]].mode;
+}
+else {
+    console.log('Invalid config mode switching to dev mode..');
+    config.port = configObj.dev.port;
+    config.mode = configObj.dev.mode;
+}
 
 module.exports = config;
