d6db44c36299b4800fe6b80eedb0cd582d0fb5a8
sudhakar reddy <sudhakar.balreddy@mindtree.com>
2017-02-27 11:47:17 +0530

    db changes

--- config/index.js
+++ config/index.js
@@ -17,7 +17,7 @@ var configObj = {
 };
 
 if(process.argv[2] === undefined || process.argv[2] === null){
-    if(process.env.NODE_ENV){
+    if(process.env.NODE_ENV === 'production'){
         config.port = process.env.PORT;
         config.mode = process.env.NODE_ENV;
     }
