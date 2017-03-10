4b52321b3c3057cafc6b9ba8d2deb82861c38a0b
sudhakar reddy <sudhakar.balreddy@mindtree.com>
2017-03-10 12:02:15 +0530

    Implemented SignUp page validations Add employee validations and also removed session expiry on page refresh

--- public/controllers/homeCtrl.js
+++ public/controllers/homeCtrl.js
@@ -1,14 +1,21 @@
-app.controller('homeCtrl', function(genService,$scope,authService,$location){
+app.controller('homeCtrl', function(genService,$scope,authService,$location,localStorageService){
     var vm = this;
     $('.dropdown-toggle').dropdown();
-    vm.showSignedUser = false;
+    if(localStorageService.get("empdirusername")){
+        vm.showSignedUser = true;
+        vm.username = localStorageService.get("empdirusername");
+    }
+    else {
+        vm.showSignedUser = false;
+    }
     $scope.$on('usernameUpdated', function (event, data) {
         vm.username = data;
         vm.showSignedUser = true;
+        localStorageService.set("empdirusername", vm.username);
     });
     vm.logoutUser = function(){
-        authService.setLogin('false');
-        authService.checkToken();
+        authService.logout();
+        localStorageService.clear();
         vm.showSignedUser = false;
     }
 });
